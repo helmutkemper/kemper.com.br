39d54a7f906c2762987eb8c95fdae9c1b6d79ea2
Helmut Kemper <helmut.kemper@gmail.com>
2021-02-27 19:56:34 -0300

    initial commit

--- main.go
+++ main.go
@@ -1,78 +1,16 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	var err error
-	var database *sql.DB
-	var statement *sql.Stmt
-	var rows *sql.Rows
-
-	database, err = sql.Open("sqlite3", "./db/database.sqlite")
-	if err != nil {
-		log.Fatalf("sql.Open().error: %v", err)
-	}
-
-	statement, err = database.Prepare(
-		"CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)",
-	)
-	if err != nil {
-		log.Fatalf("sql.Prepare().error: %v", err)
-	}
-
-	_, err = statement.Exec()
-	if err != nil {
-		log.Fatalf("sql.Exec().error: %v", err)
-	}
-
-	statement, err = database.Prepare(
-		"INSERT INTO people (firstname, lastname) VALUES (?, ?)",
-	)
-	if err != nil {
-		log.Fatalf("sql.Prepare().error: %v", err)
-	}
-
-	_, err = statement.Exec("Rob", "Gronkowski")
-	if err != nil {
-		log.Fatalf("sql.Exec().error: %v", err)
-	}
-
-	rows, err = database.Query("SELECT id, firstname, lastname FROM people")
-	if err != nil {
-		log.Fatalf("sql.Query().error: %v", err)
-	}
-
-	var id int
-	var firstname string
-	var lastname string
-	for rows.Next() {
-		err = rows.Scan(&id, &firstname, &lastname)
-		if err != nil {
-			log.Fatalf("rows.Scan().error: %v", err)
-		}
-
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
-	}
-
-	os.Exit(0)
-	//var t ToJsObject = ToJsObject{
-	//	"key": ToJsObject{
-	//		"width": "100px",
-	//	},
-	//	"key_two": 1,
-	//}
-	//t.Convert()
 
 	r := gin.Default()
 	r.StaticFS("/static", http.Dir("./static"))
