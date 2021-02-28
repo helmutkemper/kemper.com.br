020191fc5364a08bc2dec347457e98147a72d64b
Helmut Kemper <helmut.kemper@gmail.com>
2021-02-28 14:15:08 -0300

    initial commit

---
+++ server/funcReceverFormNewUser.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ReceverFormNewUser(c *gin.Context) {
+	//var err error
+	//var body []byte
+	//
+	//body, err = ioutil.ReadAll(c.Request.Body)
+	//if err != nil {
+	//	log.Printf("server.ReceverFormNewUser().error: %v", err.Error())
+	//}
+	//
+	//c.Params.Get("name")
+}
